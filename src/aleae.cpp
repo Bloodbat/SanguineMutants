9be1eb7c41b9fd48c2a04574ffc5a4055989c0b4
Bloodbat <bloodbad@prodigy.net.mx>
2024-05-14 11:19:34 -0600

    Change: >= instead of > when checking probability, like the hardware code.

--- src/aleae.cpp
+++ src/aleae.cpp
@@ -195,7 +195,7 @@ struct Aleae : Module {
 					// Trigger.
 					// Don't have to clamp here because the threshold comparison works without it.
 					float threshold = params[PARAM_THRESHOLD1 + i].getValue() + inputs[INPUT_P1 + i].getPolyVoltage(channel) / 10.f;
-					rollResults[i][channel] = (random::uniform() > threshold) ? ROLL_HEADS : ROLL_TAILS;
+					rollResults[i][channel] = (random::uniform() >= threshold) ? ROLL_HEADS : ROLL_TAILS;
 					if (rollModes[i] == ROLL_TOGGLE) {
 						rollResults[i][channel] = RollResults(lastRollResults[i][channel] ^ rollResults[i][channel]);
 					}
