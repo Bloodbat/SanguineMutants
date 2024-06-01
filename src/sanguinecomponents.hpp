96092438563d0417db7ab7cd2ffebfd373ab5b72
Bloodbat <bloodbad@prodigy.net.mx>
2024-05-31 18:46:59 -0600

    Add: make the Rogan 6PS light available to every module.

--- src/sanguinecomponents.hpp
+++ src/sanguinecomponents.hpp
@@ -187,6 +187,14 @@ struct CKD6Light : TBase {
 	}
 };
 
+template <typename Base>
+struct Rogan6PSLight : Base {
+	Rogan6PSLight() {
+		this->box.size = mm2px(Vec(23.04, 23.04));
+		this->borderColor = nvgRGBA(0, 0, 0, 0);
+	}
+};
+
 // Decorations
 
 struct SanguineShapedLight : SvgLight {
