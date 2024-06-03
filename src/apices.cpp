15746e1543fd4f3cc13a5a9f66c082d23496189e
Bloodbat <bloodbad@prodigy.net.mx>
2024-06-03 03:05:00 -0600

    Fix: use Schmitt triggers (per the voltage standards) for gates: rising edges lower than 10 V were not always accounted for.

--- src/apices.cpp
+++ src/apices.cpp
@@ -49,6 +49,7 @@ static const uint16_t kAdcThresholdUnlocked = 1 << (16 - 10);  // 10 bits
 static const uint16_t kAdcThresholdLocked = 1 << (16 - 8);  // 8 bits
 
 static const uint8_t kButtonCount = 3;
+static const uint8_t kInputCount = 2;
 
 static const std::vector<std::string> modeList{
 	"ENVELOPE",
@@ -174,6 +175,7 @@ struct Apices : Module {
 	int16_t brightness[kNumChannels] = { 0, 0 };
 
 	dsp::SchmittTrigger stSwitches[kButtonCount];
+	dsp::SchmittTrigger stInputTriggers[kInputCount];
 
 	// update descriptions/oleds every 16 samples
 	static const int kClockUpdateFrequency = 16;
@@ -279,8 +281,8 @@ struct Apices : Module {
 			}
 
 			uint32_t gateTriggers = 0;
-			gateTriggers |= (inputs[GATE_1_INPUT].getVoltage() ? 1 : 0);
-			gateTriggers |= (inputs[GATE_2_INPUT].getVoltage() ? 2 : 0);
+			gateTriggers |= (stInputTriggers[0].process(inputs[GATE_1_INPUT].getVoltage()) ? 1 : 0);
+			gateTriggers |= (stInputTriggers[1].process(inputs[GATE_2_INPUT].getVoltage()) ? 2 : 0);
 
 			uint32_t buttons = 0;
 			buttons |= (params[PARAM_TRIGGER_1].getValue() ? 1 : 0);
