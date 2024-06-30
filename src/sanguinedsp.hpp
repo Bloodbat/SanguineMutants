035762de235808b591a426132c61a24703956883
Bloodbat <bloodbad@prodigy.net.mx>
2024-06-30 00:48:30 -0600

    Add: regular float saturator.

--- src/sanguinedsp.hpp
+++ src/sanguinedsp.hpp
@@ -1,13 +1,15 @@
 #pragma once
 using simd::float_4;
 
+// Saturators: Zavalishin 2018, "The Art of VA Filter Design",
+// http://www.native-instruments.com/fileadmin/ni_media/downloads/pdf/VAFilterDesign_2.0.0a.pdf via Bogaudio
+
 struct SaturatorFloat_4 {
 	static const float_4 limit;
 
-	// Zavalishin 2018, "The Art of VA Filter Design", http://www.native-instruments.com/fileadmin/ni_media/downloads/pdf/VAFilterDesign_2.0.0a.pdf via Bogaudio
 	static inline float_4 saturation(float_4 x) {
 		const float_4 y1 = 0.98765f; // (2*x - 1)/x**2 where x is 0.9.
-		const float_4 offset = 0.075f / SaturatorFloat_4::limit; // Magic.
+		const float_4 offset = 0.075f / limit; // Magic.
 		float_4 x1 = (x + 1.0f) * 0.5f;
 		return limit * (offset + x1 - simd::sqrt(x1 * x1 - y1 * x) * (1.f / y1));
 	}
@@ -17,6 +19,25 @@ struct SaturatorFloat_4 {
 		float_4 negativeVoltage = sample < 0.f;
 		float_4 saturated = simd::ifelse(negativeVoltage, -saturation(-x), saturation(x));
 		return saturated;
+	}
+};
+
+struct SaturatorFloat {
+	static const float limit;
+
+	static inline float saturation(float x) {
+		const float y1 = 0.98765f; // (2*x - 1)/x**2 where x is 0.9.
+		const float offset = 0.075f / limit; // Magic.
+		float x1 = (x + 1.0f) * 0.5f;
+		return limit * (offset + x1 - sqrtf(x1 * x1 - y1 * x) * (1.f / y1));
+	}
+
+	float next(float sample) {
+		float x = sample * (1.f / limit);
+		if (sample < 0.f) {
+			return -saturation(-x);
+		}
+		return saturation(x);
 	}
 };
 
