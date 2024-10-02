e1ff9953e428a177fae6b6c5b1305fb603f907ba
Bloodbat <bloodbad@prodigy.net.mx>
2024-10-02 05:07:58 -0600

    Fix: disastrous peace not displaying when it is selected.

--- src/anuli.cpp
+++ src/anuli.cpp
@@ -314,7 +314,7 @@ struct Anuli : SanguineModule {
 		}
 
 		if (bDividerTurn) {
-			displayText = anuliModeLabels[resonatorModel[0]];
+			displayText = bEasterEgg[0] ? anuliModeLabels[6] : anuliModeLabels[resonatorModel[0]];
 
 			uint8_t pulseWidthModulationCounter = getSystemTimeMs() & 15;
 			uint8_t triangle = (getSystemTimeMs() >> 5) & 31;
