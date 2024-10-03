126ce6a82f614e2d7df9faa3bc79a08f4e33cce6
Bloodbat <bloodbad@prodigy.net.mx>
2024-10-03 16:17:31 -0600

    Add: lights to show polyphonic channels and their algorithm value.

--- src/incurvationes.cpp
+++ src/incurvationes.cpp
@@ -34,6 +34,7 @@ struct Incurvationes : SanguineModule {
 		ENUMS(LIGHT_CARRIER, 2),
 		ENUMS(LIGHT_ALGORITHM, 3),
 		LIGHT_EASTER_EGG,
+		ENUMS(LIGHT_CHANNEL_ALGORITHM, PORT_MAX_CHANNELS * 3),
 		LIGHTS_COUNT
 	};
 
@@ -154,22 +155,35 @@ struct Incurvationes : SanguineModule {
 			lights[LIGHT_EASTER_EGG].setBrightness(bEasterEggEnabled ? 1.f : 0.f);
 
 			const uint8_t(*palette)[3];
-			float zone;
-			if (!bEasterEggEnabled) {
-				zone = 8.0f * warpsParameters[0]->modulation_algorithm;
-				palette = paletteWarpsDefault;
-			}
-			else {
-				zone = 8.0f * warpsParameters[0]->phase_shift;
-				palette = paletteWarpsFreqsShift;
+
+			palette = bEasterEggEnabled ? paletteWarpsFreqsShift : paletteWarpsDefault;
+			float colorValues[PORT_MAX_CHANNELS][3] = {};
+
+			for (int channel = 0; channel < channelCount; channel++) {
+				float zone = 8.f * (bEasterEggEnabled ? warpsParameters[channel]->phase_shift : warpsParameters[channel]->modulation_algorithm);
+
+				const int currentLight = LIGHT_CHANNEL_ALGORITHM + channel * 3;
+				MAKE_INTEGRAL_FRACTIONAL(zone);
+				int zone_fractional_i = static_cast<int>(zone_fractional * 256);
+				for (int i = 0; i < 3; i++) {
+					int a = palette[zone_integral][i];
+					int b = palette[zone_integral + 1][i];
+
+					colorValues[channel][i] = static_cast<float>(a + ((b - a) * zone_fractional_i >> 8)) / 255.0f;
+
+					lights[currentLight + i].setBrightness(colorValues[channel][i]);
+				}
 			}
 
-			MAKE_INTEGRAL_FRACTIONAL(zone);
-			int zone_fractional_i = static_cast<int>(zone_fractional * 256);
-			for (int i = 0; i < 3; i++) {
-				int a = palette[zone_integral][i];
-				int b = palette[zone_integral + 1][i];
-				lights[LIGHT_ALGORITHM + i].setBrightness(static_cast<float>(a + ((b - a) * zone_fractional_i >> 8)) / 255.0f);
+			lights[LIGHT_ALGORITHM + 0].setBrightness(colorValues[0][0]);
+			lights[LIGHT_ALGORITHM + 1].setBrightness(colorValues[0][1]);
+			lights[LIGHT_ALGORITHM + 2].setBrightness(colorValues[0][2]);
+
+			for (int channel = channelCount; channel < PORT_MAX_CHANNELS; channel++) {
+				const int currentLight = LIGHT_CHANNEL_ALGORITHM + channel * 3;
+				lights[currentLight + 0].setBrightness(0.f);
+				lights[currentLight + 1].setBrightness(0.f);
+				lights[currentLight + 2].setBrightness(0.f);
 			}
 		}
 	}
@@ -197,10 +211,10 @@ struct IncurvationesWidget : SanguineModuleWidget {
 		addChild(createLightCentered<Rogan6PSLight<RedGreenBlueLight>>(millimetersToPixelsVec(25.4, 37.486),
 			module, Incurvationes::LIGHT_ALGORITHM));
 		addParam(createParamCentered<Rogan1PWhite>(millimetersToPixelsVec(42.388, 79.669), module, Incurvationes::PARAM_TIMBRE));
-		addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<GreenRedLight>>>(millimetersToPixelsVec(16.906, 63.862),
+		addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<GreenRedLight>>>(millimetersToPixelsVec(25.4, 63.862),
 			module, Incurvationes::PARAM_CARRIER, Incurvationes::LIGHT_CARRIER));
 
-		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(33.894, 63.862), module, Incurvationes::INPUT_ALGORITHM));
+		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(42.388, 63.862), module, Incurvationes::INPUT_ALGORITHM));
 
 		addParam(createParamCentered<Sanguine1PYellow>(millimetersToPixelsVec(8.412, 79.451), module, Incurvationes::PARAM_LEVEL1));
 		addParam(createParamCentered<Sanguine1PBlue>(millimetersToPixelsVec(25.4, 79.451), module, Incurvationes::PARAM_LEVEL2));
@@ -213,6 +227,23 @@ struct IncurvationesWidget : SanguineModuleWidget {
 		addInput(createInputCentered<BananutGreenPoly>(millimetersToPixelsVec(18.777, 112.172), module, Incurvationes::INPUT_MODULATOR));
 		addOutput(createOutputCentered<BananutRedPoly>(millimetersToPixelsVec(32.044, 112.172), module, Incurvationes::OUTPUT_MODULATOR));
 		addOutput(createOutputCentered<BananutRedPoly>(millimetersToPixelsVec(42.896, 112.172), module, Incurvationes::OUTPUT_AUX));
+
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(14.281, 62.532), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 0 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(16.398, 62.532), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 1 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(18.516, 62.532), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 2 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(20.633, 62.532), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 3 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(30.148, 62.532), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 4 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(32.265, 62.532), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 5 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(34.382, 62.532), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 6 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(36.5, 62.532), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 7 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(14.281, 65.191), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 8 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(16.398, 65.191), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 9 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(18.516, 65.191), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 10 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(20.633, 65.191), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 11 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(30.148, 65.191), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 12 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(32.265, 65.191), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 13 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(34.382, 65.191), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 14 * 3));
+		addChild(createLightCentered<TinyLight<RedGreenBlueLight>>(millimetersToPixelsVec(36.5, 65.191), module, Incurvationes::LIGHT_CHANNEL_ALGORITHM + 15 * 3));
 	}
 };
 
