966bfd94273fe30a0176c62c3e325c14b057b815
Bloodbat <bloodbad@prodigy.net.mx>
2024-10-11 16:23:04 -0600

    Fix: clearer variable names.

--- src/contextus.cpp
+++ src/contextus.cpp
@@ -519,11 +519,11 @@ struct Contextus : SanguineModule {
 				}
 
 				// Pitch transposition
-				int32_t t = 0;
+				int32_t transposition = 0;
 				// TODO!!! Fix disabled LFO: disabled in hardware and prone to crashing Rack, probably needs firmware work.
-				//t = settings[channel].pitch_range == renaissance::PITCH_RANGE_LFO ? -(36 << 7) : 0;
-				t += (static_cast<int32_t>(settings[channel].pitch_octave) - 2) * 12 * 128;
-				osc[channel].set_pitch(pitch + t);
+				//transposition = settings[channel].pitch_range == renaissance::PITCH_RANGE_LFO ? -(36 << 7) : 0;
+				transposition += (static_cast<int32_t>(settings[channel].pitch_octave) - 2) * 12 * 128;
+				osc[channel].set_pitch(pitch + transposition);
 
 				if (bTriggerFlag[channel]) {
 					osc[channel].Strike();
