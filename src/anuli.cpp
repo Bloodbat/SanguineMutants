2d8b2cedefdfb01c9457dbab182ca61537d787d5
Bloodbat <bloodbad@prodigy.net.mx>
2024-10-15 12:36:08 -0600

    Refactor: Anuli: move global constants to header.

--- src/anuli.cpp
+++ src/anuli.cpp
@@ -6,36 +6,9 @@
 #include "sanguinehelpers.hpp"
 #include "sanguinechannels.hpp"
 
+#include "anuli.hpp"
+
 #pragma GCC diagnostic ignored "-Wclass-memaccess"
-
-static const std::vector<std::string> anuliModeLabels = {
-	"Modal Reso.",
-	"Sym. Strings",
-	"M. I. String",
-	"FM voice",
-	"Q. Sym. Str.",
-	"Rev. String",
-	"Disas. Peace"
-};
-
-static const std::vector<std::string> anuliFxLabels = {
-	"Formant filter",
-	"Rolandish chorus",
-	"Caveman reverb",
-	"Formant filter (less abrasive variant)",
-	"Rolandish chorus (Solinaish ensemble)",
-	"Caveman reverb (shinier variant)"
-};
-
-static const std::vector<std::string> anuliMenuLabels = {
-	"Modal resonator",
-	"Sympathetic strings",
-	"Modulated/inharmonic string",
-	"FM voice",
-	"Quantized sympathetic strings",
-	"Reverb string",
-	"Disastrous Peace"
-};
 
 struct Anuli : SanguineModule {
 	enum ParamIds {
---
+++ src/anuli.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+static const std::vector<std::string> anuliModeLabels = {
+    "Modal Reso.",
+    "Sym. Strings",
+    "M. I. String",
+    "FM voice",
+    "Q. Sym. Str.",
+    "Rev. String",
+    "Disas. Peace"
+};
+
+static const std::vector<std::string> anuliFxLabels = {
+    "Formant filter",
+    "Rolandish chorus",
+    "Caveman reverb",
+    "Formant filter (less abrasive variant)",
+    "Rolandish chorus (Solinaish ensemble)",
+    "Caveman reverb (shinier variant)"
+};
+
+static const std::vector<std::string> anuliMenuLabels = {
+    "Modal resonator",
+    "Sympathetic strings",
+    "Modulated/inharmonic string",
+    "FM voice",
+    "Quantized sympathetic strings",
+    "Reverb string",
+    "Disastrous Peace"
+};
