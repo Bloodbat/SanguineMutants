337d9f343e261d7786748424236219624865add8
Bloodbat <bloodbad@prodigy.net.mx>
2024-10-15 14:13:44 -0600

    Refactor: Apices family: move common declarations to shared header. Refactor: Apices family: move global constants, enums and structs to header.

---
+++ src/apicescommon.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+enum SwitchIndex {
+    SWITCH_TWIN_MODE,
+    SWITCH_EXPERT,
+    SWITCH_CHANNEL_SELECT,
+    SWITCH_GATE_TRIG_1,
+    SWITCH_GATE_TRIG_2
+};
+
+enum EditMode {
+    EDIT_MODE_TWIN,
+    EDIT_MODE_SPLIT,
+    EDIT_MODE_FIRST,
+    EDIT_MODE_SECOND,
+    EDIT_MODE_LAST
+};
+
+enum LightModes {
+    LIGHT_OFF,
+    LIGHT_ON,
+    LIGHT_BLINK
+};
+
+struct Settings {
+    uint8_t editMode;
+    uint8_t processorFunction[2];
+    uint8_t potValue[8];
+    bool snap_mode;
+};
+
+struct KnobLabels {
+    std::string knob1;
+    std::string knob2;
+    std::string knob3;
+    std::string knob4;
+};
+
+static const size_t kNumBlocks = 2;
+static const size_t kNumChannels = 2;
+static const size_t kBlockSize = 4;
+static const uint8_t kNumAdcChannels = 4;
+static const uint16_t kAdcThresholdUnlocked = 1 << (16 - 10);  // 10 bits
+static const uint16_t kAdcThresholdLocked = 1 << (16 - 8);  // 8 bits
+
+static const uint8_t kButtonCount = 3;
