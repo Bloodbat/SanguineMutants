4fb53fab57820085d457c4713e32d9f461711098
Bloodbat <bloodbad@prodigy.net.mx>
2024-11-03 16:11:24 -0600

    Fix: dsp: ensure simd namespace is always found.

--- src/sanguinedsp.hpp
+++ src/sanguinedsp.hpp
@@ -6,19 +6,19 @@
 // http://www.native-instruments.com/fileadmin/ni_media/downloads/pdf/VAFilterDesign_2.0.0a.pdf via Bogaudio
 
 struct SaturatorFloat_4 {
-	static const simd::float_4 limit;
+	static const rack::simd::float_4 limit;
 
-	static inline simd::float_4 saturation(simd::float_4 x) {
-		const simd::float_4 y1 = 0.98765f; // (2*x - 1)/x**2 where x is 0.9.
-		const simd::float_4 offset = 0.075f / limit; // Magic.
-		simd::float_4 x1 = (x + 1.f) * 0.5f;
-		return limit * (offset + x1 - simd::sqrt(x1 * x1 - y1 * x) * (1.f / y1));
+	static inline rack::simd::float_4 saturation(rack::simd::float_4 x) {
+		const rack::simd::float_4 y1 = 0.98765f; // (2*x - 1)/x**2 where x is 0.9.
+		const rack::simd::float_4 offset = 0.075f / limit; // Magic.
+		rack::simd::float_4 x1 = (x + 1.f) * 0.5f;
+		return limit * (offset + x1 - rack::simd::sqrt(x1 * x1 - y1 * x) * (1.f / y1));
 	}
 
-	simd::float_4 next(simd::float_4 sample) {
-		simd::float_4 x = sample * (1.f / limit);
-		simd::float_4 negativeVoltage = sample < 0.f;
-		simd::float_4 saturated = simd::ifelse(negativeVoltage, -saturation(-x), saturation(x));
+	rack::simd::float_4 next(rack::simd::float_4 sample) {
+		rack::simd::float_4 x = sample * (1.f / limit);
+		rack::simd::float_4 negativeVoltage = sample < 0.f;
+		rack::simd::float_4 saturated = rack::simd::ifelse(negativeVoltage, -saturation(-x), saturation(x));
 		return saturated;
 	}
 };
@@ -42,4 +42,4 @@ struct SaturatorFloat {
 	}
 };
 
-const simd::float_4 SaturatorFloat_4::limit = 12.f;
+const rack::simd::float_4 SaturatorFloat_4::limit = 12.f;
