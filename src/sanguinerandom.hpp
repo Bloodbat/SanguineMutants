836a4e676b51efc5d31e459ddd1a9363caf15478
Bloodbat <bloodbad@prodigy.net.mx>
2025-04-06 07:17:42 -0600

    Add: sanguine random: random generator adapted from Mutable Instruments' stmlib.

--- src/sanguinerandom.hpp
+++ src/sanguinerandom.hpp
@@ -13,4 +13,32 @@ namespace sanguineRandom {
         float theta = kDoublePi * rack::random::get<float>();
         return radius * std::sin(theta);
     }
+
+    // Adapted from Mutable Instruments' stmlib.
+    class MutableRandom {
+    public:
+        inline uint32_t state() {
+            return rng_state_;
+        }
+
+        inline void Seed(uint32_t seed) {
+            rng_state_ = seed;
+        }
+
+        inline uint32_t GetWord() {
+            rng_state_ = rng_state_ * 1664525L + 1013904223L;
+            return state();
+        }
+
+        inline int16_t GetSample() {
+            return static_cast<int16_t>(GetWord() >> 16);
+        }
+
+        inline float GetFloat() {
+            return static_cast<float>(GetWord()) / 4294967296.0f;
+        }
+
+    private:
+        uint32_t rng_state_;
+    };
 }
