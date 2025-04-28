b284a15eb6d8a569eba5d042dc2bbee5bfbe926f
Bloodbat <bloodbad@prodigy.net.mx>
2025-04-27 19:13:00 -0600

    Refactor: Aestus: use rescale to convert peacock voltages: center them and make them bigger.

--- src/aestus.cpp
+++ src/aestus.cpp
@@ -257,8 +257,8 @@ struct Aestus : SanguineModule {
 			if (++frame >= 16) {
 				frame = 0;
 				plotter.Run();
-				outputs[OUTPUT_UNI].setVoltage((static_cast<float>(plotter.x()) / static_cast<float>(UINT16_MAX)) * 8.f);
-				outputs[OUTPUT_BI].setVoltage((static_cast<float>(-plotter.y()) / static_cast<float>(UINT16_MAX)) * 8.f);
+				outputs[OUTPUT_UNI].setVoltage(rescale(static_cast<float>(plotter.x()), 0.f, UINT16_MAX, -10.f, 10.f));
+				outputs[OUTPUT_BI].setVoltage(rescale(static_cast<float>(plotter.y()), 0.f, UINT16_MAX, 10.f, -10.f));
 			}
 
 			if (bIsLightsTurn) {
