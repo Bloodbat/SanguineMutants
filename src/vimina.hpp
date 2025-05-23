32a7809a354901f26e725b5c8c3f3e734132d0a2
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-05-22 18:48:13 -0600

    Add: Vimina: show current swing or active clock division or multiplication factors in the knob's tooltip using the description field.

--- src/vimina.hpp
+++ src/vimina.hpp
@@ -1,8 +1,28 @@
-#pragma once
+﻿#pragma once
 
 namespace vimina {
     static const std::vector<std::string> modeLabels = {
         "Clock multiplier/divider",
         "Clock swing"
     };
+
+    static const std::vector<std::string> factorLabels = {
+        "x1",
+        "x2",
+        "x3",
+        "x4",
+        "x5",
+        "x6",
+        "x7",
+        "x8",
+        "÷2",
+        "÷3",
+        "÷4",
+        "÷5",
+        "÷6",
+        "÷7",
+        "÷8"
+    };
+
+    static const std::string clockPrefix = "Clock: ";
 }
