a5ffe6f9a2f3d8d4201b24409cd1fb9ed1caf13f
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-06-17 03:11:25 -0600

    Change: Funes firmware: format source.

--- eurorack/plaits/dsp/engine/chord_engine.h
+++ eurorack/plaits/dsp/engine/chord_engine.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
@@ -35,40 +35,31 @@
 #include "plaits/dsp/oscillator/wavetable_oscillator.h"
 
 namespace plaits {
+    const int kChordNumHarmonics = 3;
 
-const int kChordNumHarmonics = 3;
+    class ChordEngine : public Engine {
+    public:
+        ChordEngine() {}
+        ~ChordEngine() {}
 
-class ChordEngine : public Engine {
- public:
-  ChordEngine() { }
-  ~ChordEngine() { }
-  
-  virtual void Init(stmlib::BufferAllocator* allocator) override;
-  virtual void Reset() override;
-  virtual void LoadUserData(const uint8_t* user_data) override { }
-  virtual void Render(const EngineParameters& parameters,
-      float* out,
-      float* aux,
-      size_t size,
-      bool* already_enveloped) override;
+        virtual void Init(stmlib::BufferAllocator* allocator) override;
+        virtual void Reset() override;
+        virtual void LoadUserData(const uint8_t* user_data) override {}
+        virtual void Render(const EngineParameters& parameters, float* out, float* aux, size_t size,
+            bool* already_enveloped) override;
 
- private:
-  void ComputeRegistration(float registration, float* amplitudes);
-  int ComputeChordInversion(
-      float inversion,
-      float* ratios,
-      float* amplitudes);
-  
-  StringSynthOscillator divide_down_voice_[kChordNumVoices];
-  WavetableOscillator<128, 15> wavetable_voice_[kChordNumVoices];
-  ChordBank chords_;
-  
-  float morph_lp_;
-  float timbre_lp_;
-  
-  DISALLOW_COPY_AND_ASSIGN(ChordEngine);
-};
+    private:
+        void ComputeRegistration(float registration, float* amplitudes);
+        int ComputeChordInversion(float inversion, float* ratios, float* amplitudes);
 
+        StringSynthOscillator divide_down_voice_[kChordNumVoices];
+        WavetableOscillator<128, 15> wavetable_voice_[kChordNumVoices];
+        ChordBank chords_;
+
+        float morph_lp_;
+        float timbre_lp_;
+
+        DISALLOW_COPY_AND_ASSIGN(ChordEngine);
+    };
 }  // namespace plaits
-
 #endif  // PLAITS_DSP_ENGINE_CHORD_ENGINE_H_
