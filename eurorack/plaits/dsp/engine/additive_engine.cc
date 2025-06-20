f3c9c34ed41f71308a0d1f97f3c1f7222a85d27e
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-06-20 02:02:20 -0600

    Remove: Funes firmware: (static analysis): unused method.

--- eurorack/plaits/dsp/engine/additive_engine.cc
+++ eurorack/plaits/dsp/engine/additive_engine.cc
@@ -92,12 +92,6 @@ namespace plaits {
     }
   }
 
-  inline float Bump(float x, float centroid, float slope) {
-    float d = fabsf(x - centroid);
-    float bump = 1.0f - d * slope;
-    return bump + fabsf(bump);
-  }
-
   const int integer_harmonics[24] = {
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
     16, 17, 18, 19, 20, 21, 22, 23
