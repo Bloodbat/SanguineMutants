b8f423cb583934be12259106cbbe14a0a7f04396
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-06-30 02:18:39 -0600

    Change: Nebulae family firmware: format sources.

--- alt_firmware/fluctus/dsp/fluctus_kammerl_player.cc
+++ alt_firmware/fluctus/dsp/fluctus_kammerl_player.cc
@@ -3,47 +3,44 @@
 #include "fluctus/dsp/fluctus_kammerl_player.h"
 
 namespace fluctus {
+	float KammerlPlayer::quantizeSize(float size) const {
+		if (size >= 1.0f) {
+			return 1.0f;
+		}
+		if (size <= 0.0f) {
+			return 0.0f;
+		}
+		static const float kSizeQuantizationBorder[] = {
+				1.0f / 32.0f,
+				(1.0f / 32.0f + 1.0f / 16.0f) / 2.0f,
+				(1.0f / 16.0f + 1.0f / 8.0f) / 2.0f,
+				(1.0f / 8.0f + 1.0f / 4.0f) / 2.0f,
+				(1.0f / 4.0f + 1.0f / 3.0f) / 2.0f,
+				(1.0f / 3.0f + 1.0f / 2.0f) / 2.0f,
+				(1.0f / 2.0f + 3.0f / 4.0f) / 2.0f,
+				(3.0f / 4.0f + 1.0f) / 2.0f, 1.0f };
+		static const float kSizeQuantization[] = {
+				1.0f / 32.0f,
+				1.0f / 32.0f,
+				1.0f / 16.0f,
+				1.0f / 8.0f,
+				1.0f / 4.0f,
+				1.0f / 3.0f,
+				1.0f / 2.0f,
+				3.0f / 4.0f,
+				1.0f };
 
-float KammerlPlayer::quantizeSize(float size) const {
-	if (size >= 1.0f) {
-		return 1.0f;
-	}
-	if (size <= 0.0f) {
-		return 0.0f;
-	}
-	static const float kSizeQuantizationBorder[] = {
-			1.0f / 32.0f,
-			(1.0f / 32.0f + 1.0f / 16.0f) / 2.0f,
-			(1.0f / 16.0f + 1.0f / 8.0f) / 2.0f,
-			(1.0f / 8.0f + 1.0f / 4.0f) / 2.0f,
-			(1.0f / 4.0f + 1.0f / 3.0f) / 2.0f,
-			(1.0f / 3.0f + 1.0f / 2.0f) / 2.0f,
-			(1.0f / 2.0f + 3.0f / 4.0f) / 2.0f,
-			(3.0f / 4.0f + 1.0f) / 2.0f, 1.0f };
-	static const float kSizeQuantization[] = {
-			1.0f / 32.0f,
-			1.0f / 32.0f,
-			1.0f / 16.0f,
-			1.0f / 8.0f,
-			1.0f / 4.0f,
-			1.0f / 3.0f,
-			1.0f / 2.0f,
-			3.0f / 4.0f,
-			1.0f };
-
-	static const size_t kNumSizeQuantizationIntevals = sizeof(kSizeQuantization)
-			/ sizeof(float);
-	if (size > kSizeQuantizationBorder[0]) {
-		size_t index = std::upper_bound(kSizeQuantizationBorder,
-				kSizeQuantizationBorder + kNumSizeQuantizationIntevals, size)
-				- kSizeQuantizationBorder;
+		static const size_t kNumSizeQuantizationIntevals = sizeof(kSizeQuantization) /
+			sizeof(float);
+		if (size > kSizeQuantizationBorder[0]) {
+			size_t index = std::upper_bound(kSizeQuantizationBorder,
+				kSizeQuantizationBorder + kNumSizeQuantizationIntevals, size) - kSizeQuantizationBorder;
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wtype-limits"
-		CONSTRAIN(index, 0, kNumSizeQuantizationIntevals - 1);
+			CONSTRAIN(index, 0, kNumSizeQuantizationIntevals - 1);
 #pragma GCC diagnostic pop
-		return kSizeQuantization[index];
+			return kSizeQuantization[index];
+		}
+		return size;
 	}
-	return size;
-}
-
 }  // namespace fluctus
