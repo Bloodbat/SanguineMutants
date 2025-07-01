c3716b1ec45f34f33d66e72a5dbeeb90fb87cc80
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-07-01 03:15:15 -0600

    Change: Funes firmware: format sources.

--- eurorack/plaits/dsp/engine/wavetable_engine.h
+++ eurorack/plaits/dsp/engine/wavetable_engine.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
@@ -33,48 +33,44 @@
 #include "plaits/dsp/oscillator/wavetable_oscillator.h"
 
 namespace plaits {
+  class WavetableEngine : public Engine {
+  public:
+    WavetableEngine() {}
+    ~WavetableEngine() {}
 
-class WavetableEngine : public Engine {
- public:
-  WavetableEngine() { }
-  ~WavetableEngine() { }
-  
-  virtual void Init(stmlib::BufferAllocator* allocator) override;
-  virtual void Reset() override;
-  virtual void LoadUserData(const uint8_t* user_data) override;
-  virtual void Render(const EngineParameters& parameters,
-      float* out,
-      float* aux,
-      size_t size,
+    virtual void Init(stmlib::BufferAllocator* allocator) override;
+    virtual void Reset() override;
+    virtual void LoadUserData(const uint8_t* user_data) override;
+    virtual void Render(const EngineParameters& parameters, float* out, float* aux, size_t size,
       bool* already_enveloped) override;
-  
- private:
-  float ReadWave(int x, int y, int z, int phase_i, float phase_f);
-   
-  float phase_;
-  
-  float x_pre_lp_;
-  float y_pre_lp_;
-  float z_pre_lp_;
-  
-  float x_lp_;
-  float y_lp_;
-  float z_lp_;
 
-  float previous_x_;
-  float previous_y_;
-  float previous_z_;
-  float previous_f0_;
-  
-  // Maps a (bank, X, Y) coordinate to a waveform index.
-  // This allows all waveforms to be reshuffled by the user to create new maps.
-  const int16_t** wave_map_;
-  
-  Differentiator diff_out_;
-  
-  DISALLOW_COPY_AND_ASSIGN(WavetableEngine);
-};
+  private:
+    float ReadWave(int x, int y, int z, int phase_i, float phase_f);
 
+    float phase_;
+
+    float x_pre_lp_;
+    float y_pre_lp_;
+    float z_pre_lp_;
+
+    float x_lp_;
+    float y_lp_;
+    float z_lp_;
+
+    float previous_x_;
+    float previous_y_;
+    float previous_z_;
+    float previous_f0_;
+
+    /*
+       Maps a (bank, X, Y) coordinate to a waveform index.
+       This allows all waveforms to be reshuffled by the user to create new maps.
+    */
+    const int16_t** wave_map_;
+
+    Differentiator diff_out_;
+
+    DISALLOW_COPY_AND_ASSIGN(WavetableEngine);
+  };
 }  // namespace plaits
-
 #endif  // PLAITS_DSP_ENGINE_WAVETABLE_ENGINE_H_
