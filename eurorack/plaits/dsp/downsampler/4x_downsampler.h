da4e5af18ae951ba2fea239e16a5de419f958237
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-07-01 03:15:15 -0600

    Change: Funes firmware: format sources.

--- eurorack/plaits/dsp/downsampler/4x_downsampler.h
+++ eurorack/plaits/dsp/downsampler/4x_downsampler.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
@@ -34,38 +34,35 @@
 #include "plaits/resources.h"
 
 namespace plaits {
-  
-const size_t kOversampling = 4;
+  const size_t kOversampling = 4;
 
-class Downsampler {
- public:
-  Downsampler(float* state) {
-    head_ = *state;
-    tail_ = 0.0f;
-    state_ = state;
-  }
-  ~Downsampler() {
-    *state_ = head_;
-  }
-  inline void Accumulate(int i, float sample) {
-    head_ += sample * lut_4x_downsampler_fir[3 - (i & 3)];
-    tail_ += sample * lut_4x_downsampler_fir[i & 3];
-  }
+  class Downsampler {
+  public:
+    Downsampler(float* state) {
+      head_ = *state;
+      tail_ = 0.0f;
+      state_ = state;
+    }
+    ~Downsampler() {
+      *state_ = head_;
+    }
+    inline void Accumulate(int i, float sample) {
+      head_ += sample * lut_4x_downsampler_fir[3 - (i & 3)];
+      tail_ += sample * lut_4x_downsampler_fir[i & 3];
+    }
 
-  inline float Read() {
-    float value = head_;
-    head_ = tail_;
-    tail_ = 0.0f;
-    return value;
-  }
- private:
-  float head_;
-  float tail_;
-  float* state_;
+    inline float Read() {
+      float value = head_;
+      head_ = tail_;
+      tail_ = 0.0f;
+      return value;
+    }
+  private:
+    float head_;
+    float tail_;
+    float* state_;
 
-  DISALLOW_COPY_AND_ASSIGN(Downsampler);
-};
-
+    DISALLOW_COPY_AND_ASSIGN(Downsampler);
+  };
 }  // namespace plaits
-
 #endif  // PLAITS_DSP_DOWNSAMPLER_4X_DOWNSAMPLER_H_
--- eurorack/plaits/dsp/engine2/wave_terrain_engine.h
+++ eurorack/plaits/dsp/engine2/wave_terrain_engine.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
@@ -40,36 +40,31 @@
 #include "plaits/dsp/oscillator/sine_oscillator.h"
 
 namespace plaits {
-  
-class WaveTerrainEngine : public Engine {
- public:
-  WaveTerrainEngine() { }
-  ~WaveTerrainEngine() { }
-  
-  virtual void Init(stmlib::BufferAllocator* allocator) override;
-  virtual void Reset() override;
-  virtual void LoadUserData(const uint8_t* user_data) override {
-    user_terrain_ = (const int8_t*)(user_data);
-  }
-  virtual void Render(const EngineParameters& parameters,
-      float* out,
-      float* aux,
-      size_t size,
-      bool* already_enveloped) override;
-  
- private:
-  float Terrain(float x, float y, int terrain_index);
-  
-  FastSineOscillator path_;
-  float offset_;
-  float terrain_;
-  
-  float* temp_buffer_;
-  const int8_t* user_terrain_;
-  
-  DISALLOW_COPY_AND_ASSIGN(WaveTerrainEngine);
-};
+  class WaveTerrainEngine : public Engine {
+  public:
+    WaveTerrainEngine() {}
+    ~WaveTerrainEngine() {}
 
+    virtual void Init(stmlib::BufferAllocator* allocator) override;
+    virtual void Reset() override;
+
+    virtual void LoadUserData(const uint8_t* user_data) override {
+      user_terrain_ = (const int8_t*)(user_data);
+    }
+
+    virtual void Render(const EngineParameters& parameters, float* out, float* aux, size_t size, bool* already_enveloped) override;
+
+  private:
+    float Terrain(float x, float y, int terrain_index);
+
+    FastSineOscillator path_;
+    float offset_;
+    float terrain_;
+
+    float* temp_buffer_;
+    const int8_t* user_terrain_;
+
+    DISALLOW_COPY_AND_ASSIGN(WaveTerrainEngine);
+  };
 }  // namespace plaits
-
 #endif  // PLAITS_DSP_ENGINE_WAVE_TERRAIN_ENGINE_H_
