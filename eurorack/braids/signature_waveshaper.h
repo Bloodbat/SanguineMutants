f13f492f11acf378dbce8469c37e5af5a52153ad
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-07-02 13:55:35 -0600

    Change: Nodi family firmware: format sources.

--- eurorack/braids/signature_waveshaper.h
+++ eurorack/braids/signature_waveshaper.h
@@ -36,61 +36,58 @@
 #include "braids/resources.h"
 
 namespace braids {
+  class SignatureWaveshaper {
+  public:
+    SignatureWaveshaper() {}
+    ~SignatureWaveshaper() {}
 
-class SignatureWaveshaper {
- public:
-  SignatureWaveshaper() { }
-  ~SignatureWaveshaper() { }
+    inline void Init(uint32_t seed) {
+      int32_t skew = seed & 15;
+      seed >>= 4;
 
-  inline void Init(uint32_t seed) {
-    int32_t skew = seed & 15;
-    seed >>= 4;
+      int32_t sigmoid_strength = seed & 31;
+      seed >>= 5;
 
-    int32_t sigmoid_strength = seed & 31;
-    seed >>= 5;
+      int32_t bumplets_frequency = seed & 3;
+      seed >>= 2;
+      bumplets_frequency += 3;
 
-    int32_t bumplets_frequency = seed & 3;
-    seed >>= 2;
-    bumplets_frequency += 3;
+      int32_t bumplets_width = seed & 7;
+      seed >>= 3;
+      bumplets_width += 1;
+      bumplets_width <<= 7;
+      bumplets_width *= bumplets_width;
 
-    int32_t bumplets_width = seed & 7;
-    seed >>= 3;
-    bumplets_width += 1;
-    bumplets_width <<= 7;
-    bumplets_width *= bumplets_width;
+      for (int i = 0; i < 256; ++i) {
+        int16_t x = (i - 128) << 8;
+        int16_t x_skew = i * i - 32768;
+        x = stmlib::Mix(x, x_skew, skew << 11);
 
-    for (int i = 0; i < 256; ++i) {
-      int16_t x = (i - 128) << 8;
-      int16_t x_skew = i * i - 32768;
-      x = stmlib::Mix(x, x_skew, skew << 11);
-
-      int16_t sigmoid = x * (8192 + (sigmoid_strength << 10)) / \
+        int16_t sigmoid = x * (8192 + (sigmoid_strength << 10)) / \
           (8192 + (sigmoid_strength * abs(x) >> 5));
-      int16_t bumplets = wav_sine[(i * bumplets_frequency) & 255];
-      uint16_t bumplet_gain = x * x / (bumplets_width) + 16;
-      bumplet_gain = 32768 * 128 / (128 + bumplet_gain);
-      transfer_[i] = stmlib::Mix(sigmoid, bumplets, bumplet_gain);
+        int16_t bumplets = wav_sine[(i * bumplets_frequency) & 255];
+        uint16_t bumplet_gain = x * x / (bumplets_width)+16;
+        bumplet_gain = 32768 * 128 / (128 + bumplet_gain);
+        transfer_[i] = stmlib::Mix(sigmoid, bumplets, bumplet_gain);
+      }
+      transfer_[256] = transfer_[255];
     }
-    transfer_[256] = transfer_[255];
-  }
 
-  inline int32_t transfer(uint16_t i) {
-    return transfer_[i];
-  }
+    inline int32_t transfer(uint16_t i) {
+      return transfer_[i];
+    }
 
-  inline int32_t Transform(int16_t sample) {
-    uint16_t i = sample + 32768;
-    int32_t a = transfer_[i >> 8];
-    int32_t b = transfer_[(i >> 8) + 1];
-    return a + ((b - a) * (i & 0xff) >> 8);
-  }
+    inline int32_t Transform(int16_t sample) {
+      uint16_t i = sample + 32768;
+      int32_t a = transfer_[i >> 8];
+      int32_t b = transfer_[(i >> 8) + 1];
+      return a + ((b - a) * (i & 0xff) >> 8);
+    }
 
- private:
-  int32_t transfer_[257];
+  private:
+    int32_t transfer_[257];
 
-  DISALLOW_COPY_AND_ASSIGN(SignatureWaveshaper);
-};
-
+    DISALLOW_COPY_AND_ASSIGN(SignatureWaveshaper);
+  };
 }  // namespace braids
-
 #endif // BRAIDS_VCO_JITTER_SOURCE_H_
