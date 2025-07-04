0654494ef054bc3123c547144c5db6f10e43cef1
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-07-04 04:41:33 -0600

    Change: Nebulae family: better buffer related constant names.

--- src/fluctus.cpp
+++ src/fluctus.cpp
@@ -186,16 +186,15 @@ struct Fluctus : SanguineModule {
 		lastHiFi = 1;
 		lastStereo = 1;
 
-		const int memLen = 118784;
-		const int ccmLen = 65536 - 128;
-		bufferLarge = new uint8_t[memLen]();
-		bufferSmall = new uint8_t[ccmLen]();
+		const int bigBufferLength = 118784;
+		const int ccmBufferLength = 65536 - 128;
+		bufferLarge = new uint8_t[bigBufferLength]();
+		bufferSmall = new uint8_t[ccmBufferLength]();
 		fluctusProcessor = new fluctus::FluctusGranularProcessor();
 		memset(fluctusProcessor, 0, sizeof(*fluctusProcessor));
+		fluctusProcessor->Init(bufferLarge, bigBufferLength, bufferSmall, ccmBufferLength);
 
 		lightsDivider.setDivision(kClockDivider);
-
-		fluctusProcessor->Init(bufferLarge, memLen, bufferSmall, ccmLen);
 	}
 
 	~Fluctus() {
