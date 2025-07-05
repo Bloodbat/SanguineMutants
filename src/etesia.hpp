684a9882776af0a1f2050a0eee591b418c8cdc98
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-07-04 19:31:16 -0600

    Change: Nebulae family: better parameter tooltips. Change: Nebulae family: OLED display contents tweaks.

--- src/etesia.hpp
+++ src/etesia.hpp
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "cloudycommon.hpp"
 
@@ -13,20 +13,20 @@ namespace etesia {
     };
 
     static const std::vector<cloudyCommon::ParasiteModeDisplay> modeDisplays{
-        {"Freeze",  "Position",     "Density",          "Size",             "Texture",           "Pitch",     "Trigger", "Blend",      "Spread",    "Feedback",   "Reverb"},
-        {"Stutter", "Scrub",        "Diffusion",        "Overlap",          "LP/HP",             "Pitch",     "Time",    "Blend",      "Spread",    "Feedback",   "Reverb"},
-        {"Stutter", "Time / Start", "Diffusion",        "Overlap / Duratn", "LP/HP",             "Pitch",     "Time",    "Blend",      "Spread",    "Feedback",   "Reverb"},
-        {"Freeze",  "Buffer",       "FFT Upd. / Merge", "Polynomial",       "Quantize / Parts",  "Transpose", "Glitch",  "Blend",      "Spread",    "Feedback",   "Reverb"},
-        {"Freeze",  "Pre-delay",    "Decay",            "Size",             "Dampen LP-V ?-HP",  "Pitch",     "Clock",   "Dry/Wet",    "Diffusion", "Mod. Speed", "Mod. Amount"},
-        {"Voice",   "Timbre",       "Decay",            "Chord",            "Filter LP-V ?-BP",  "Pitch",     "Burst",   "Distortion", "Stereo",    "Harmonics",  "Scatter"}
+        {"Freeze",  "Position",     "Density",          "Size",             "Texture",          "Pitch",     "Trigger", "Blend",      "Spread",    "Feedback",   "Reverb"},
+        {"Stutter", "Scrub",        "Diffusion",        "Overlap",          "LP | HP",          "Pitch",     "Time",    "Blend",      "Spread",    "Feedback",   "Reverb"},
+        {"Stutter", "Time | Start", "Diffusion",        "Overlap | Duratn", "LP | HP",          "Pitch",     "Time",    "Blend",      "Spread",    "Feedback",   "Reverb"},
+        {"Freeze",  "Buffer",       "FFT Upd. | Merge", "Polynomial",       "Quantize | Parts", "Transpose", "Glitch",  "Blend",      "Spread",    "Feedback",   "Reverb"},
+        {"Freeze",  "Pre-delay",    "Decay",            "Size",             "Dampen LP | HP",   "Pitch",     "Clock",   "Dry | Wet",  "Diffusion", "Mod. Speed", "Mod. Amount"},
+        {"Voice",   "Timbre",       "Decay",            "Chord",            "Filter LP | BP",   "Pitch",     "Burst",   "Distortion", "Stereo",    "Harmonics",  "Scatter"}
     };
 
     static const std::vector<cloudyCommon::ParasiteModeDisplay> modeTooltips{
-        {"Freeze",  "Position",     "Density",            "Size",               "Texture",          "Pitch",     "Trigger", "Blend",      "Spread",    "Feedback",         "Reverb"},
-        {"Stutter", "Scrub",        "Diffusion",          "Overlap",            "LP/HP",            "Pitch",     "Time",    "Blend",      "Spread",    "Feedback",         "Reverb"},
-        {"Stutter", "Time / Start", "Diffusion",          "Overlap / Duration", "LP/HP",            "Pitch",     "Time",    "Blend",      "Spread",    "Feedback",         "Reverb"},
-        {"Freeze",  "Buffer",       "FFT Update / Merge", "Polynomial",         "Quantize / Parts", "Transpose", "Glitch",  "Blend",      "Spread",    "Feedback",         "Reverb"},
-        {"Freeze",  "Pre-delay",    "Decay",              "Size",               "Dampening",        "Pitch",     "Clock",   "Dry/Wet",    "Diffusion", "Modulation speed", "Modulation amount"},
-        {"Voice",   "Timbre",       "Decay",              "Chord",              "Filter",           "Pitch",     "Burst",   "Distortion", "Stereo",    "Harmonics",        "Scatter"}
+        {"Freeze",          "Grain position",                   "Grain density",                "Grain Size",                   "Grain texture",                        "Grain pitch",  "Trigger",          "Dry ↔ wet",    "Spread",       "Feedback",         "Reverb"},
+        {"Stutter",         "Scrub buffer",                     "Diffusion",                    "Overlap",                      "Filter: LP ↓ | HP ↑",                  "Grain pitch",  "Time",             "Dry ↔ wet",    "Spread",       "Feedback",         "Reverb"},
+        {"Stutter",         "Head position",                    "Granular diffusion",           "Overlap: Grainy ↓ | Smooth ↑", "Filter: LP ↓ | HP ↑",                  "Grain pitch",  "Delay time",       "Dry ↔ wet",    "Spread",       "Feedback",         "Reverb"},
+        {"Freeze",          "Buffer select",                    "F.F.T. Update ↓ | Merge ↑",    "Polynomial coefficients",      "Quantizer ↓ | Partial amplifier ↑",    "Transpose",    "Glitch audio",     "Dry ↔ wet",    "Spread",       "Feedback",         "Reverb"},
+        {"Freeze",          "Pre-delay Multiply ↓ | Divide ↑",  "Decay",                        "Room size",                    "Dampening: LP ↓ | HP ↑",               "Pitch shift",  "Clock",            "Dry ↔ wet",    "Diffusion",    "Modulation speed", "Modulation amount"},
+        {"Switch voice",    "Timbre",                           "Decay",                        "Chord",                        "Filter: LP ↓ | HP ↑",                  "Pitch",        "Burst",            "Distortion",   "Stereo mix",   "String harmonics", "Scatter"}
     };
 }
