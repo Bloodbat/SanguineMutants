49d4bf11e18a563e5be621f0331bcb6d2e20fad3
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-07-11 00:33:51 -0600

    Remove: Nebulae family: unused string constant.

--- src/cloudycommon.hpp
+++ src/cloudycommon.hpp
@@ -21,8 +21,6 @@ namespace cloudyCommon {
 		std::string menuLabel;
 	};
 
-	static const std::string kCVSuffix = " CV";
-
 	static const std::string kLedButtonPrefix = "LED display value: ";
 
 	static const std::vector<std::string> buttonTexts{
