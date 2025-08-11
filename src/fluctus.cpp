83505e76ccd45aecf31364dc1d06c6d7b7c899be
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-08-11 15:55:29 -0600

    Refactor: Nebulae family: more SIMD leveraging for handling modulations.

--- src/fluctus.cpp
+++ src/fluctus.cpp
@@ -233,15 +233,18 @@ struct Fluctus : SanguineModule {
 #endif
 		bool bHaveModeCable = inputs[INPUT_MODE].isConnected();
 
-		float paramBlend = params[PARAM_BLEND].getValue();
-		float paramSpread = params[PARAM_SPREAD].getValue();
-		float paramFeedback = params[PARAM_FEEDBACK].getValue();
-		float paramReverb = params[PARAM_REVERB].getValue();
+		float_4 knobValues;
+		float_4 sliderValues;
 
-		float paramPosition = params[PARAM_POSITION].getValue();
-		float paramDensity = params[PARAM_DENSITY].getValue();
-		float paramSize = params[PARAM_SIZE].getValue();
-		float paramTexture = params[PARAM_TEXTURE].getValue();
+		knobValues[0] = params[PARAM_BLEND].getValue();
+		knobValues[1] = params[PARAM_SPREAD].getValue();
+		knobValues[2] = params[PARAM_FEEDBACK].getValue();
+		knobValues[3] = params[PARAM_REVERB].getValue();
+
+		sliderValues[0] = params[PARAM_POSITION].getValue();
+		sliderValues[1] = params[PARAM_DENSITY].getValue();
+		sliderValues[2] = params[PARAM_SIZE].getValue();
+		sliderValues[3] = params[PARAM_TEXTURE].getValue();
 
 		float paramPitch = params[PARAM_PITCH].getValue();
 
@@ -322,10 +325,7 @@ struct Fluctus : SanguineModule {
 
 				scaledVoltages /= 5.f;
 
-				scaledVoltages[0] += paramBlend;
-				scaledVoltages[1] += paramSpread;
-				scaledVoltages[2] += paramFeedback;
-				scaledVoltages[3] += paramReverb;
+				scaledVoltages += knobValues;
 
 				scaledVoltages = clamp(scaledVoltages, 0.f, 1.f);
 
@@ -336,10 +336,7 @@ struct Fluctus : SanguineModule {
 
 				scaledVoltages = voltages1[channel] / 5.f;
 
-				scaledVoltages[0] += paramPosition;
-				scaledVoltages[1] += paramDensity;
-				scaledVoltages[2] += paramSize;
-				scaledVoltages[3] += paramTexture;
+				scaledVoltages += sliderValues;
 
 				scaledVoltages = clamp(scaledVoltages, 0.f, 1.f);
 
