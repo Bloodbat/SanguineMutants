c68263b7e378303a6ef86dd4dab4cc34bb623e4f
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-08-20 20:48:06 -0600

    Change: Funes: reorder constant.

--- src/Funes.cpp
+++ src/Funes.cpp
@@ -86,6 +86,8 @@ struct Funes : SanguineModule {
 
 	static const int kMaxUserDataSize = 4096;
 
+	static const int kBlockSize = 12;
+
 	int frequencyMode = 10;
 	int displayModelNum = 0;
 
@@ -93,8 +95,6 @@ struct Funes : SanguineModule {
 
 	int channelCount = 0;
 	int errorTimeOut = 0;
-
-	static const int kBlockSize = 12;
 
 	uint8_t chordBank = 0;
 
