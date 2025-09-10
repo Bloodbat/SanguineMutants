7779954db6966c3abb5f7accbe098f18a77f5b0b
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-09-10 01:51:19 -0600

    Refactor: Funes: set output converter sample rate when engine sample rate changes instead of on every process.

--- src/Funes.cpp
+++ src/Funes.cpp
@@ -339,7 +339,6 @@ struct Funes : SanguineModule {
 
 			// Convert output.
 			if (!bWantLowCpu) {
-				srcOutputs.setRates(48000, static_cast<int>(args.sampleRate));
 				int inLen = kBlockSize;
 				int outLen = drbOutputBuffers.capacity();
 				srcOutputs.setChannels(channelCount * 2);
@@ -480,6 +479,10 @@ struct Funes : SanguineModule {
 				break;
 			}
 		}
+	}
+
+	void onSampleRateChange(const SampleRateChangeEvent& e) override {
+		srcOutputs.setRates(48000, static_cast<int>(e.sampleRate));
 	}
 
 	json_t* dataToJson() override {
