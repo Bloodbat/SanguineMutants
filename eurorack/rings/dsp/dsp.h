5c447a5a7c5c86874032bca5a000dc65e7890fcb
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-10-10 00:35:34 -0600

    Refactor: Anuli: use firmware constant for sample rate.

--- eurorack/rings/dsp/dsp.h
+++ eurorack/rings/dsp/dsp.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
@@ -35,11 +35,8 @@
 #define BRYAN_CHORDS
 
 namespace rings {
-  
-static const float kSampleRate = 48000.0f;
-const float a3 = 440.0f / kSampleRate;
-const size_t kMaxBlockSize = 24;
-
+    static const float kSampleRate = 48000.0f;
+    const float a3 = 440.0f / kSampleRate;
+    const size_t kMaxBlockSize = 24;
 }  // namespace rings
-
 #endif  // RINGS_DSP_DSP_H_
