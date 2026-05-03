963a43abe119f25b0af758e43eec973bc9943ee2
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2026-05-02 22:47:10 -0600

    Fix: Tides2 firmware: comments. Change: Tides2 firmware: format sources.

--- eurorack/tides2/ramp/ratio.h
+++ eurorack/tides2/ramp/ratio.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,25 +19,21 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
-//
-// Clock division ratio
 
+// Clock division ratio
 #ifndef TIDES_RAMP_RATIO_H_
 #define TIDES_RAMP_RATIO_H_
 
 #include "stmlib/stmlib.h"
 
 namespace tides2 {
-
-struct Ratio {
-  float ratio;
-  int q;
-};
-
-}  // namespace tides
-
+  struct Ratio {
+    float ratio;
+    int q;
+  };
+}  // namespace tides2
 #endif  // TIDES_RAMP_RATIO_H_
--- eurorack/tides2/resources.h
+++ eurorack/tides2/resources.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,37 +19,34 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
-//
-// Resources definitions.
-//
-// Automatically generated with:
-// make resources
 
+/*
+Resources definitions.
+
+Automatically generated with:
+make resources
+*/
 
 #ifndef TIDES2_RESOURCES_H_
 #define TIDES2_RESOURCES_H_
 
-
 #include "stmlib/stmlib.h"
 
-
-
 namespace tides2 {
+    typedef uint8_t ResourceId;
 
-typedef uint8_t ResourceId;
+    extern const float* lookup_table_table[];
 
-extern const float* lookup_table_table[];
+    extern const int16_t* lookup_table_i16_table[];
 
-extern const int16_t* lookup_table_i16_table[];
-
-extern const float lut_sine[];
-extern const float lut_bipolar_fold[];
-extern const float lut_unipolar_fold[];
-extern const int16_t lut_wavetable[];
+    extern const float lut_sine[];
+    extern const float lut_bipolar_fold[];
+    extern const float lut_unipolar_fold[];
+    extern const int16_t lut_wavetable[];
 #define LUT_SINE 0
 #define LUT_SINE_SIZE 1281
 #define LUT_BIPOLAR_FOLD 1
@@ -59,6 +56,5 @@ extern const int16_t lut_wavetable[];
 #define LUT_WAVETABLE 0
 #define LUT_WAVETABLE_SIZE 12300
 
-}  // namespace tides
-
+}  // namespace tides2
 #endif  // TIDES2_RESOURCES_H_
